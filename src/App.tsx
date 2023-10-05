d1a62a4e8a4abf90666945d2572397cc187a7db5
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-10-05 15:29:02 -0300

    controles de um player de video com useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import Button from './Button';
 import Input from './Input';
 import Checkbox from './Checkbox';
 import Exercicio from './exercicios/exercicioUseState/ExerciciouseState';
+import videoSrc from './video.mp4';
 
 // tipando caso o button recebesse o total e setTotal
 type ButtonExample = {
@@ -18,6 +19,33 @@ function App() {
         setTotal(total => total + 1);
     }
 
+    const video = useRef<HTMLVideoElement>(null);
+    const [playing, setPlaying] = useState(false);
+
+    function forward() {
+        if (!video.current) return;
+        video.current.currentTime += 2;
+    }
+
+    function changePlayBackRate(speed: number) {
+        if (!video.current) return;
+        video.current.playbackRate = speed;
+    }
+
+    function mute() {
+        if (!video.current) return;
+        video.current.muted = !video.current.muted;
+    }
+
+    async function pictureInPicture() {
+        if (!video.current) return;
+        if (document.pictureInPictureElement) {
+            await document.exitPictureInPicture();
+        } else {
+            await video.current.requestPictureInPicture();
+        }
+    }
+
     return (
         <div>
             <p>Total: {total}</p>
@@ -29,6 +57,7 @@ function App() {
             >
                 Incrementar
             </Button>
+
             {/* Exercício Input */}
             <p>Início da Viagem: {data}</p>
             <Input label="Nome" id="nome" />
@@ -40,11 +69,36 @@ function App() {
                 id="date"
                 type="date"
             />
+
             {/* Eventos */}
             <Checkbox label="Termos e Condições" />
+
             {/* Exercício useState */}
             <p>Exercício useState</p>
             <Exercicio />
+
+            {/* useRef */}
+            <div className="flex">
+                {playing ? (
+                    <button onClick={() => video.current?.pause()}>
+                        Pause
+                    </button>
+                ) : (
+                    <button onClick={() => video.current?.play()}>Play</button>
+                )}
+                <button onClick={forward}>+ 2s</button>
+                <button onClick={() => changePlayBackRate(1)}>1x</button>
+                <button onClick={() => changePlayBackRate(2)}>2x</button>
+                <button onClick={pictureInPicture}>PiP</button>
+                <button onClick={mute}>Mute</button>
+            </div>
+            <video
+                controls
+                ref={video}
+                src={videoSrc}
+                onPlay={() => setPlaying(true)}
+                onPause={() => setPlaying(false)}
+            ></video>
         </div>
     );
 }
